9f96bd95d3dcebea9f4a6efacab296fd21a528ed
Eryk <35810@ma-web.nl>
2022-11-22 10:43:46 +0100

    finsihed

---
+++ les1/app.js
@@ -0,0 +1,26 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!");
+        let appNaam = "Carfix";
+        let versienummer = 1.4;
+        let versiedatum = new Date("2002-10-25");
+        let autheur = "Eryk";
+        let copyright = "CC";
+        let distributeur = "Eryk";
+        let darkmode = true;
+        
+        console.log(appNaam);
+        console.log(versienummer);
+        console.log(versiedatum);
+        console.log(autheur);
+        console.log(copyright);
+        console.log(distributeur);
+        console.log(darkmode);
+    }
+    
+}
+
+let app = new App();
+app.runApplication();
